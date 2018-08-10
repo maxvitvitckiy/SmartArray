d57d197610ddf4c1de7e63e6968ee3f7762e7ef8
wispeckiy <max.vitvitckiy@gmail.com>
2018-08-10 13:47:52 +0300

    s

--- SmartArray/SmartArray/main.cpp
+++ SmartArray/SmartArray/main.cpp
@@ -1,4 +1,5 @@
 #include "iostream"
+#include "time.h"
 using namespace std;
 
 class SmartDoubleArray 
@@ -12,14 +13,25 @@ public:
 	{
 		arr = new double[5];
 		capacity = 5;
-		size = 0;
+		size = 5;
 		
+		srand(time(NULL));
+
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 10;
+		}
 	}
 	SmartDoubleArray(int cap) 
 	{
+		srand(time(NULL));
 		arr = new double[cap];
 		capacity = cap;
-		size = 0;
+		size = cap;
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 10;
+		}
 	}
 	SmartDoubleArray(double *arr)
 	{
@@ -43,7 +55,7 @@ public:
 		this->capacity = cap;
 	}
 
-	void Add(double value) 
+	void Add(double value)   // вставить элемент со значением value в конец массива
 	{
 		if (size < capacity) 
 		{
@@ -60,14 +72,17 @@ public:
 			}
 			newarr[size] = value;
 			size++;
-			delete[]arr;
+			delete [] arr;
 			arr = new double[capacity];
-			arr = newarr;
-			delete[] newarr;
+			for (int i = 0; i < size; i++)
+			{
+				arr[i] = newarr[i];
+			}
+			delete [] newarr;
 		}
 	}
 
-	void Insert(double value, int index)
+	void Insert(double value, int index)   // вставить элемент со значением value после индекса index
 	{
 		if (index >= size - 1)
 		{
@@ -104,11 +119,16 @@ public:
 			}
 			delete[]arr;
 			arr = new double[capacity];
-			arr = newarr;
+
+			for (int i = 0; i < size; i++)
+			{
+				arr[i] = newarr[i];
+			}
+			delete[] newarr;
 		}
 	}
 
-	void Remove(int index) 
+	void Remove(int index)  // удалить элемент с индексом index
 	{
 		for (int i = index; i < size; i++)
 		{
@@ -122,7 +142,7 @@ public:
 		return arr[index];
 	}
 
-	int Count(double value) 
+	int Count(double value) // посчитать количество элементов со значением value
 	{
 		int count = 0;
 		for (int i = 0; i < size; i++)
@@ -131,8 +151,8 @@ public:
 		}
 		return count;
 	}
-
-	int FindNext(double value, int index)
+	
+	int FindNext(double value, int index)  // найти след. значение value после индекса index
 	{
 		for (int i = index + 1; i < size; i++)
 		{
@@ -146,14 +166,24 @@ public:
 		return size;
 	}
 
-	int GetCapacity()
+	int GetCapacity()  
 	{
 		return capacity;
 	}
 
-	// []
-	
-	// predicate
+	void ShowArray() 
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+	double &operator[](int j) // перегрузка []     
+	{
+		return arr[j];
+	}
 
 	~SmartDoubleArray() 
 	{
@@ -161,7 +191,89 @@ public:
 	}
 };
 
+bool IsEmpty(SmartDoubleArray arr)
+{
+	return arr.GetSize() == 0;
+}
+
+void Menu(SmartDoubleArray arr);
+
 void main()
 {
+	SmartDoubleArray *My_Smart_Array = new SmartDoubleArray();
+	Menu(*My_Smart_Array);
 
+	system("Pause");
 }
+
+
+
+void Menu(SmartDoubleArray arr) 
+{
+	int ch;
+	double value;
+	int index;
+	do {
+		cout
+			<< "1 - Add element" << endl
+			<< "2 - Insert element after index" << endl
+			<< "3 - Get element by index" << endl
+			<< "4 - Get count of some element in the array" << endl
+			<< "5 - Find next element" << endl
+			<< "6 - Get size" << endl
+			<< "7 - Get capacity" << endl
+			<< "8 - Show array" << endl
+			<< "9 - Check if array is empty" << endl
+			<< "0 - Exit" << endl;
+		cin >> ch;
+		cout << endl;
+		switch (ch)
+		{
+		case 1: 
+			cout << "Value: ";
+			cin >> value;
+			arr.Add(value);
+			break;
+		case 2: 
+			cout << "Value: ";
+			cin >> value;
+			cout << "Index: ";
+			cin >> index;
+			arr.Insert(value, index);
+			break;
+		case 3:
+			cout << "Index: ";
+			cin >> index;
+			cout << arr[index] << endl;
+			break;
+		case 4:
+			cout << "Value: ";
+			cin >> value;
+			cout << "Count of elements: " << arr.Count(value) << endl;
+			break;
+		case 5: 
+			cout << "Value: ";
+			cin >> value;
+			cout << "Index: ";
+			cin >> index;
+			cout << "Next element index: " << arr.FindNext(value, index) << endl;
+			break;
+		case 6:
+			cout << "Size: " <<arr.GetSize() << endl;
+			break;
+		case 7:
+			cout << "Capacity: " << arr.GetCapacity() << endl;
+			break;
+		case 8: 
+			cout << "Array: ";
+			arr.ShowArray();
+			break;
+		case 9: 
+			if (IsEmpty(arr) == true) cout << "Empty is empty." << endl;
+			else cout << "Empty is not empty." << endl;
+			break;
+		}
+		cout << endl;
+	} while (ch != 0);
+}
+
